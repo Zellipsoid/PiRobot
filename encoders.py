199221dcbf0ed837823ba9c7f4bf12a2a77c8877
zellipsoid <zach44zach@gmail.org>
2018-09-06 10:59:24 -0400

    some fixes

--- encoders.py
+++ encoders.py
@@ -17,13 +17,13 @@ class Encoders(object):
     # #tempStartTime = time.time();
 
     def __init__(self):
-        velArrayRight = []
-        velArrayLeft = []
-        rightTicks = 0
-        leftTicks = 0
-        tempRightTicks = 0
-        tempLeftTicks = 0
-        startTime = time.time()
+        self.velArrayRight = []
+        self.velArrayLeft = []
+        self.rightTicks = 0
+        self.leftTicks = 0
+        self.tempRightTicks = 0
+        self.tempLeftTicks = 0
+        self.startTime = time.time()
 
     # This function is called when the left encoder detects a rising edge signal.
     def onLeftEncode(self, pin):
@@ -57,25 +57,28 @@ class Encoders(object):
         return (self.leftTicks, self.rightTicks)
 
     def getSpeeds(self):
-        pprint(self.velArrayLeft)
-        pprint(self.velArrayRight)
+        totalTime = self.getElapsedTime()
+        #pprint(self.velArrayLeft)
+        #pprint(self.velArrayRight)
         leftLength = len(self.velArrayLeft)
         rightLength = len(self.velArrayRight)
         # return(
         # (velArrayLeft[1][leftLength - 1] - velArrayLeft[1][leftLength - 2]) / (velArrayLeft[0][leftLength - 1] - velArrayLeft[0][leftLength - 2]),
         # (velArrayRight[1][rightLength - 1] - velArrayRight[1][rightLength - 2]) / (velArrayRight[0][rightLength - 1] - velArrayRight[0][rightLength - 2]))
-        if rightLength > 0 and leftLength > 0:
+        if rightLength > 0 and leftLength > 0 and totalTime > 0:
             self.totalTimeLeft = 0
             self.totalTicksLeft = 0
-            for i in self.velArrayLeft:
-                self.totalTimeLeft += self.velArrayLeft[0][i]
-                self.totalTicksLeft += self.velArrayLeft[1][i] 
+            for (time, ticks) in self.velArrayLeft:
+                self.totalTimeLeft += time
+                self.totalTicksLeft += ticks 
+                #print(val)
             self.totalTimeRight = 0
             self.totalTicksRight = 0
-            for i in self.velArrayRight:
-                self.totalTimeRight += self.velArrayRight[0][i]
-                self.totalTicksRight += self.velArrayRight[1][i]
-            return (self.totalTicksLeft / self.totalTimeLeft / 32, self.totalTicksRight / self.totalTimeRight / 32)
+            for (time, ticks) in self.velArrayRight:
+                self.totalTimeRight += time
+                self.totalTicksRight += ticks
+                #print(val)
+            return (self.totalTicksLeft / totalTime / 32, self.totalTicksRight / totalTime / 32)
         else:
             return (0, 0)
 
--- main.py
+++ main.py
@@ -37,3 +37,6 @@ GPIO.add_event_detect(RENCODER, GPIO.RISING, enc.onRightEncode)
 while True:
     print("running")
     time.sleep(1)
+    speedTuple = enc.getSpeeds()
+    print(str((speedTuple[0] + speedTuple[1]) / 2) + " RPS")
+    #print(enc.getElapsedTime())
