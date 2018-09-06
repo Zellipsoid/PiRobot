dd3b53a2fe9707e74f07d2ba5ea0d65270b57787
Zachary Figueroa <zach44zach@gmail.com>
2018-09-05 22:40:21 -0400

    Made a class for encoders

--- encoders.py
+++ encoders.py
@@ -5,34 +5,8 @@ import math
 import pprint
 
 
-velArrayRight = []
-velArrayLeft = []
-rightTicks = 0
-leftTicks = 0
-tempRightTicks = 0
-tempLeftTicks = 0
-startTime = time.time()
-#tempStartTime = time.time();
+class Encoders(object):
 
-# This function is called when the left encoder detects a rising edge signal.
-def onLeftEncode(pin):
-    print("Left encoder ticked!")
-    leftTicks += 1
-    tempLeftTicks += 1
-    velArrayLeft.append((time.time(), leftTicks))
-    if (len(velArrayLeft) > 5):
-        velArrayLeft = velArrayLeft[1:6]
-
-# This function is called when the right encoder detects a rising edge signal.
-def onRightEncode(pin):
-    print("Right encoder ticked!")
-    rightTicks += 1
-    tempRightTicks += 1
-    velArrayRight.append((time.time(), rightTicks))
-    if (len(velArrayRight) > 5):
-        velArrayRight = velArrayRight[1:6]
-
-def initEncoders():
     velArrayRight = []
     velArrayLeft = []
     rightTicks = 0
@@ -40,34 +14,65 @@ def initEncoders():
     tempRightTicks = 0
     tempLeftTicks = 0
     startTime = time.time()
+    #tempStartTime = time.time();
 
-def resetCounts():
-    rightTicks = 0
-    leftTicks = 0
+    def __init__(self):
+        velArrayRight = []
+        velArrayLeft = []
+        rightTicks = 0
+        leftTicks = 0
+        tempRightTicks = 0
+        tempLeftTicks = 0
+        startTime = time.time()
 
-def resetTempCounts():
-    tempRightTicks = 0
-    tempLeftTicks = 0
+    # This function is called when the left encoder detects a rising edge signal.
+    def onLeftEncode(self, pin):
+        print("Left encoder ticked!")
+        leftTicks += 1
+        tempLeftTicks += 1
+        velArrayLeft.append((time.time(), leftTicks))
+        if (len(velArrayLeft) > 24):
+            velArrayLeft = velArrayLeft[1:25]
 
-def getCounts():
-    return (leftTicks, rightTicks)
+    # This function is called when the right encoder detects a rising edge signal.
+    def onRightEncode(self, pin):
+        print("Right encoder ticked!")
+        rightTicks += 1
+        tempRightTicks += 1
+        velArrayRight.append((time.time(), rightTicks))
+        if (len(velArrayRight) > 24):
+            velArrayRight = velArrayRight[1:25]
 
-def getSpeeds():
-    pprint(velArrayLeft)
-    pprint(velArrayRight)
-    leftLength = len(velArrayLeft)
-    rightLength = len(velArrayRight)
-    return(
-    (velArrayLeft[1][leftLength - 1] - velArrayLeft[1][leftLength - 2]) / (velArrayLeft[0][leftLength - 1] - velArrayLeft[0][leftLength - 2]),
-    (velArrayRight[1][rightLength - 1] - velArrayRight[1][rightLength - 2]) / (velArrayRight[0][rightLength - 1] - velArrayRight[0][rightLength - 2]))
-    
+    def resetCounts(self):
+        rightTicks = 0
+        leftTicks = 0
 
+    def resetTempCounts(self):
+        tempRightTicks = 0
+        tempLeftTicks = 0
 
-def getElapsedTime():
-    return time.time() - startTime
+    def getCounts(self):
+        return (leftTicks, rightTicks)
 
-#while True:
-#    time.sleep(0.25)
-#    resetCounts()
-#    tempStartTime = time.time()
+    def getSpeeds(self):
+        pprint(velArrayLeft)
+        pprint(velArrayRight)
+        leftLength = len(velArrayLeft)
+        rightLength = len(velArrayRight)
+        # return(
+        # (velArrayLeft[1][leftLength - 1] - velArrayLeft[1][leftLength - 2]) / (velArrayLeft[0][leftLength - 1] - velArrayLeft[0][leftLength - 2]),
+        # (velArrayRight[1][rightLength - 1] - velArrayRight[1][rightLength - 2]) / (velArrayRight[0][rightLength - 1] - velArrayRight[0][rightLength - 2]))
+        totalTimeLeft = 0
+        totalTicksLeft = 0
+        for i in velArrayLeft:
+            totalTimeLeft += velArrayLeft[0][i]
+            totalTicksLeft += velArrayLeft[1][i] 
+        totalTimeRight = 0
+        totalTicksRight = 0
+        for i in velArrayRight:
+            totalTimeRight += velArrayRight[0][i]
+            totalTicksRight += velArrayRight[1][i]
+        return (totalTicksLeft / totalTimeLeft / 32, totalTicksRight)
 
+    def getElapsedTime(self):
+        return time.time() - startTime
