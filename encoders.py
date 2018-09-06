cb27ff8550255432ca2ff7839e82d9f2d0d10ad9
Zachary Figueroa <zach44zach@gmail.com>
2018-09-06 16:09:20 -0400

    I think our code was wrong before, trying to fix it

--- encoders.py
+++ encoders.py
@@ -7,51 +7,33 @@ import pprint
 
 class Encoders(object):
 
-    # velArrayRight = []
-    # velArrayLeft = []
-    # rightTicks = 0
-    # leftTicks = 0
-    # tempRightTicks = 0
-    # tempLeftTicks = 0
-    # startTime = time.time()
-    # #tempStartTime = time.time();
-
     def __init__(self):
         self.velArrayRight = []
         self.velArrayLeft = []
         self.rightTicks = 0
         self.leftTicks = 0
-        self.tempRightTicks = 0
-        self.tempLeftTicks = 0
         self.startTime = time.time()
+        self.speedRecord = 10
 
     # This function is called when the left encoder detects a rising edge signal.
     def onLeftEncode(self, pin):
         print("Left encoder ticked!")
-        self.leftTicks 
         self.leftTicks += 1
-        self.tempLeftTicks += 1
         self.velArrayLeft.append((time.time(), self.leftTicks))
-        if (len(self.velArrayLeft) > 24):
-            self.velArrayLeft = self.velArrayLeft[1:25]
+        if (len(self.velArrayLeft) > self.speedRecord):
+            self.velArrayLeft = self.velArrayLeft[1:(self.speedRecord + 1)]
 
     # This function is called when the right encoder detects a rising edge signal.
     def onRightEncode(self, pin):
         print("Right encoder ticked!")
-        self.rightTicks 
         self.rightTicks += 1
-        self.tempRightTicks += 1
         self.velArrayRight.append((time.time(), self.rightTicks))
-        if (len(self.velArrayRight) > 24):
-            self.velArrayRight = self.velArrayRight[1:25]
+        if (len(self.velArrayRight) > self.speedRecord):
+            self.velArrayRight = self.velArrayRight[1:(self.speedRecord + 1)]
 
     def resetCounts(self):
         self.rightTicks = 0
         self.leftTicks = 0
-
-    def resetTempCounts(self):
-        self.tempRightTicks = 0
-        self.tempLeftTicks = 0
 
     def getCounts(self):
         return (self.leftTicks, self.rightTicks)
@@ -67,19 +49,23 @@ class Encoders(object):
         # (velArrayLeft[1][leftLength - 1] - velArrayLeft[1][leftLength - 2]) / (velArrayLeft[0][leftLength - 1] - velArrayLeft[0][leftLength - 2]),
         # (velArrayRight[1][rightLength - 1] - velArrayRight[1][rightLength - 2]) / (velArrayRight[0][rightLength - 1] - velArrayRight[0][rightLength - 2]))
         if rightLength > 0 and leftLength > 0 and totalTime > 0:
-            self.totalTimeLeft = 0
-            self.totalTicksLeft = 0
-            for (time, ticks) in self.velArrayLeft:
-                self.totalTimeLeft += time
-                self.totalTicksLeft += ticks 
-                #print(val)
-            self.totalTimeRight = 0
-            self.totalTicksRight = 0
-            for (time, ticks) in self.velArrayRight:
-                self.totalTimeRight += time
-                self.totalTicksRight += ticks
-                #print(val)
-            return (self.totalTicksLeft / totalTime / 32 * moving[0], self.totalTicksRight / totalTime / 32 * moving[1])
+            # I think all of this was wrong
+            # self.totalTimeLeft = 0
+            # self.totalTicksLeft = 0
+            # for (time, ticks) in self.velArrayLeft:
+            #     self.totalTimeLeft += time
+            #     self.totalTicksLeft += ticks 
+            #     #print(val)
+            # self.totalTimeRight = 0
+            # self.totalTicksRight = 0
+            # for (time, ticks) in self.velArrayRight:
+            #     self.totalTimeRight += time
+            #     self.totalTicksRight += ticks
+            #     #print(val)
+            speedLeft = (velArrayLeft[1][leftLength] - velArrayLeft[1][0]) / (velArrayLeft[0][leftLength] - velArrayLeft[0][0])
+            speedRight = (velArrayRight[1][rightLength] - velArrayRight[1][0]) / (velArrayRight[0][rightLength] - velArrayRight[0][0])
+            # return (self.totalTicksLeft / totalTime / 32 * moving[0], self.totalTicksRight / totalTime / 32 * moving[1])
+            return (speedLeft * moving[0], speedRight * moving[1])
         else:
             return (0, 0)
 
