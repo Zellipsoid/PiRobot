a19c87bc80e1453f34d454fc594164048e3d0c46
Zachary Figueroa <zach44zach@gmail.com>
2018-09-06 13:54:26 -0400

    possibly fixed constructor?

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 import signal
 
 #objects for servos, encoders, sensors, and camera
-enc = encoders.encoders("ticky")
+enc = encoders.__init__("ticky")
 
 # Pins that the encoders are connected to
 LENCODER = 17
