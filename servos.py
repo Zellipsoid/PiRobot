e430e74b3403052eea198edc244de5be2d526c55
bdpfeiffer <42818948+bdpfeiffer@users.noreply.github.com>
2018-09-11 21:03:58 -0400

    Update servos.py
    added commented out versions of setSpeedsvw, ips, and rps, since they need proper calculations

--- servos.py
+++ servos.py
@@ -22,16 +22,39 @@ class Servos(object):
             right = 1.6
         self.pwm.set_pwm(self.LSERVO, 0, math.floor(left / 20 * 4096))
         self.pwm.set_pwm(self.RSERVO, 0, math.floor((3 - right) / 20 * 4096))
-    #def setSpeedsIPS(self, left, right):
-        # print("left: " + str(left))
-        # print("right: " + str(right))
-        #if left == 0 and right == 0:
-             #left = 1.5
-             #right = 1.5
-        #elif 
+    #def setSpeedsIPS(self, ipsLeft, ipsRight):
+        # print("ipsLeft: " + str(ipsLeft))
+        # print("ipsRight: " + str(ipsRight))
+        #if ipsLeft == 0 and ipsRight == 0:
+             #ipsLeft = 1.5
+             #ipsRight = 1.5
+        #else:
+            #calculate ips to ms
+            #self.setSpeeds(..., ...) #based on calibrate speeds measurements 
         #self.pwm.set_pwm(self.LSERVO, 0, math.floor(left / 20 * 4096))
         #self.pwm.set_pwm(self.RSERVO, 0, math.floor((3 - right) / 20 * 4096))
 
 # def setSpeedsRPS(rpsLeft, rpsRight):
+        # print("rpsLeft: " + str(rpsLeft))
+        # print("rpsRight: " + str(rpsRight))
+        #if rpsLeft == 0 and rpsRight == 0:
+             #rpsLeftt = 1.5
+             #rpsRight = 1.5
+        #else:
+            #calculate rps to ms
+            #self.setSpeeds(..., ...) #based on calibrate speeds measurements 
+        #self.pwm.set_pwm(self.LSERVO, 0, math.floor(left / 20 * 4096))
+        #self.pwm.set_pwm(self.RSERVO, 0, math.floor((3 - right) / 20 * 4096))
 
 # def setSpeedsvw(v, w):
+        # print("v: " + str(v))
+        # print("w: " + str(w))
+        #if v == 0 and w == 0:
+             #v = 1.5
+             #w = 1.5
+        #else:
+            #vL = v - wd #d being partial derivative of what?
+            #vR = v + wd
+            #self.setSpeeds(vL, vR) #based on calibrate speeds measurements 
+        #self.pwm.set_pwm(self.LSERVO, 0, math.floor(left / 20 * 4096))
+        #self.pwm.set_pwm(self.RSERVO, 0, math.floor((3 - right) / 20 * 4096))
